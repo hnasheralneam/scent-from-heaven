4f6772f6b270b6825fcb8cc1926dc280e49a7ffb
Hamza <hnasheralneam@gmail.com>
2024-08-12 20:14:32 -0400

    Pick up ingredients, new layout and world borders

--- scripts/ingredient.gd
+++ scripts/ingredient.gd
@@ -1,23 +1,22 @@
 extends Area2D
 
 
-@export var lerp_speed: float = 0.04 # speed
+@export var lerp_speed: float = 0.07 # speed
+@onready var following_player: bool = false
 
 func _ready():
 	pass
 
 func _process(delta):
-	var player_position: Vector2 = PlayerPosition.position
-	var player_direction = PlayerPosition.direction
+	if following_player:
+		var player_position: Vector2 = PlayerPosition.position
+		var player_direction = PlayerPosition.direction
 
+		var x_pos = 20 if player_direction == -1 else -20
+		var target_position: Vector2 = player_position + Vector2(x_pos, -8)
 
-
-	var x_pos = 20 if player_direction == -1 else -20
-	var target_position: Vector2 = player_position + Vector2(x_pos, -8)
-
-	self.set_global_position(self.global_position.lerp(target_position, lerp_speed))
+		self.set_global_position(self.global_position.lerp(target_position, lerp_speed))
 
 
 func _on_body_entered(body):
-	self.reparent(body)
-	print("collision")
+	following_player = true
